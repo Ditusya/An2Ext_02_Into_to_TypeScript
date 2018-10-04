18aed554ef37a9e48c5107e03e40af3263f0727e
Olga <ditusya@gmail.com>
2018-10-04 14:40:10 +0300

    'task2'

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,37 @@
-showHello('greeting', 'TypeScript');
+// showHello('greeting', 'TypeScript');
+//
+// function showHello(divName: string, name: string) {
+//     const elt = document.getElementById(divName);
+//     elt.innerText = `Hello from ${name}`;
+// }
 
-function showHello(divName: string, name: string) {
-    const elt = document.getElementById(divName);
-    elt.innerText = `Hello from ${name}`;
-}
+
 
 interface bookInfo {
     title: string;
     author: string;
     available: boolean;
+    category: Category;
+}
+enum Category {
+    JavaScript,
+    CSS,
+    HTML,
+    TypeScript,
+    Angular
 }
 
 function getAllBooks(){
     let allBooks:bookInfo[] = [
-        { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true},
-        { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false },
-        { title: 'CSS Secrets', author: 'Lea Verou', available: true },
-        { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true }
+        { title: 'Refactoring JavaScript', author: 'Evan Burchard', available: true, category: Category.JavaScript },
+        { title: 'JavaScript Testing', author: 'Liang Yuxian Eugene', available: false, category: Category.JavaScript},
+        { title: 'CSS Secrets', author: 'Lea Verou', available: true, category: Category.CSS },
+        { title: 'Mastering JavaScript Object-Oriented Programming', author: 'Andrea Chiarelli', available: true, category: Category.JavaScript }
     ];
     return allBooks;
 }
+
+logFirstAvailable(getAllBooks());
 
 function logFirstAvailable(allBooks:bookInfo[]): void{
     console.log(allBooks.length);
@@ -34,5 +46,19 @@ function logFirstAvailable(allBooks:bookInfo[]): void{
     }
 }
 
-console.log(logFirstAvailable(getAllBooks()));
+function getBookTitlesByCategory(category : Category){
+    let jsBooks: Array<String> = new Array;
+    for (let book of  getAllBooks()) {
+        if (book.category == category){
+            jsBooks.push(book.title);
+        }
+    }
+    return jsBooks;
+}
 
+console.log(getBookTitlesByCategory(Category.JavaScript))
+
+function logBookTitles(books: Array<String>): void{
+    books.forEach(element => console.log(element));
+}
+logBookTitles(getBookTitlesByCategory(Category.JavaScript));
